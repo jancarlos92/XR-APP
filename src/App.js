6a60787506e4e0ad3d4ea42911abb19626dce3f4
Jancarlos Acevedo <Jancarlos92@me.com>
2022-08-31 19:16:47 -0400

    time square

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-import skyscene from './assets/6.jpeg'
+import riverbed from './assets/6.jpeg'
+import pier from './assets/pier-on-the-beach.jpeg'
+import park from './assets/360-photo-panorama-.jpeg'
+import timesquare from './assets/times-square-nyc-360-view.jpeg'
+import timethumb from './assets/timesquarefont.png'
 
 function App() {
   return (
     <div className="App">
 
+
         <a-scene>
-    
-      <a-sky id="image-360" radius="10" src={skyscene}></a-sky>
-    </a-scene>
+    <a-assets>
+      <img alt="a" id="timesquare-thumb" crossorigin="anonymous" src={timethumb}/>
+      <img alt="b" id="cubes-thumb" crossorigin="anonymous" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg"/>
+      <img alt="c" id="sechelt-thumb" crossorigin="anonymous" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg"/>
+      <audio id="click-sound" crossorigin="anonymous" src="https://cdn.aframe.io/360-image-gallery-boilerplate/audio/click.ogg"></audio>
+      <img alt="e" id="pier" crossorigin="anonymous" src={pier}/>
+      <img alt="f" id="park" crossorigin="anonymous" src={park}/>
+      <img alt="g" id="timesquare" crossorigin="anonymous" src={timesquare}/>
+
+    </a-assets>
+
+
+    <a-sky id="image-360" radius="10" src="#pier"
+           animation__fade="property: components.material.material.color; type: color; from: #FFF; to: #000; dur: 300; startEvents: fade"
+           animation__fadeback="property: components.material.material.color; type: color; from: #000; to: #FFF; dur: 300; startEvents: animationcomplete__fade"></a-sky>
+
+
+    <a-entity id="links" layout="type: line; margin: 2.5" position="0 -2 -4">
+      <a-entity template="src: #link" data-src="#park" data-thumb="#park-thumb"></a-entity>
+      <a-entity template="src: #link" data-src="#pier" data-thumb="#pier-thumb"></a-entity>
+      <a-entity template="src: #link" data-src="#timesquare" data-thumb="#timesquare-thumb"></a-entity>
+    </a-entity>
+
+
+    <a-entity camera look-controls>
+      <a-cursor
+        id="cursor"
+        animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150"
+        animation__fusing="property: fusing; startEvents: fusing; from: 1 1 1; to: 0.1 0.1 0.1; dur: 1500"
+        event-set__mouseenter="_event: mouseenter; color: springgreen"
+        event-set__mouseleave="_event: mouseleave; color: black"
+        raycaster="objects: .link"></a-cursor>
+    </a-entity>
+  </a-scene>
+
 
 
     </div>
@@ -23,11 +60,11 @@ function App() {
 //     <audio id="click-sound" src="https://cdn.aframe.io/360-image-gallery-boilerplate/audio/click.ogg"></audio>
 //
 //     <!-- Images. -->
-//     <img id="city" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg">
+//     <img id="pier" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg">
 //     <img id="city-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-city.jpg">
-//     <img id="cubes" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/cubes.jpg">
+//     <img id="park" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/cubes.jpg">
 //     <img id="cubes-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg">
-//     <img id="sechelt" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg">
+//     <img id="timesquare" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg">
 //     <img id="sechelt-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg">
 //   </a-assets>
 //
